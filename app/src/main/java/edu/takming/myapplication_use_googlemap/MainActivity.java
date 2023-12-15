2b3f01ec0ddd58832c1be76446d0595f7500c270
hgtmay58 <hgtmay58@gmail.com>
2023-12-15 13:10:38 +0800

    Initial commit

---
+++ app/src/main/java/edu/takming/myapplication_use_googlemap/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.takming.myapplication_use_googlemap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
